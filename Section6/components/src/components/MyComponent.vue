456c465c414ee669c99e3e103f6a60c0dab6d248
lym <186>
2023-03-24 17:08:51 +0800

    6

---
+++ Section6/components/src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      text: '',
+      displayText: ''
+    }
+  },
+  methods: {
+    showText() {
+      this.displayText = this.text
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="text" />
+    <button @click="showText">显示</button>
+    <div v-if="displayText">{{ displayText }}</div>
+  </div>
+</template>
+
+
+
+<style>
+button {
+  margin-left: 10px;
+}
+</style>
