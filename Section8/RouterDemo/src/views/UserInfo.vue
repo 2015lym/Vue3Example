bef9ff7efbe02b009c7522aa4cbe76f1d2e52a6a
lym <186>
2023-05-23 17:54:29 +0800

    8

---
+++ Section8/RouterDemo/src/views/UserInfo.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-card>
+    <div class="user-info-card">
+      <div class="avatar">
+        <img :src="user.avatar" alt="Avatar">
+      </div>
+      <div class="user-details">
+        <h3>{{ user.name }}</h3>
+        <p>{{ user.email }}</p>
+        <p>Age: {{ user.age }}</p>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+interface User {
+  name: string;
+  email: string;
+  age: number;
+  avatar: string;
+}
+
+const user = ref<User>({
+  name: '张三',
+  email: 'zhangsan@example.com',
+  age: 28,
+  avatar: 'https://avatars.githubusercontent.com/u/16334445?v=4',
+});
+</script>
+
+<style scoped>
+.user-info-card {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+}
+
+.avatar img {
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.user-details {
+  margin-left: 20px;
+}
+</style>
---
+++ Section8/RouterDemo/src/views/UserList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-table :data="userList" border>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="email" label="Email"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+interface User {
+  name: string;
+  email: string;
+  age: number;
+}
+
+const userList: User[] = reactive([
+  { name: '张三', email: 'zhangsan@example.com', age: 28 },
+  { name: '李四', email: 'lisi@example.com', age: 32 },
+  { name: '王五', email: 'wangwu@example.com', age: 27 },
+  { name: 'John Doe', email: 'john@example.com', age: 25 },
+  { name: 'Jane Smith', email: 'jane@example.com', age: 30 },
+]);
+</script>
